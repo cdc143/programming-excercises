990411738b524efd1035ad6efbb5b17074088659
Christian Daniel <cdc143@gmail.com>
2018-07-12 22:37:21 -0400

    Add some comments

--- max_profit.py
+++ max_profit.py
@@ -1,3 +1,10 @@
+#Takes in list of ints as argument (which is assumed to be sorted "chronologically")
+#Returns a tuple in the form (max profit, buy date, sell date)
+#Returned buy and sell indicies are incremented by one (so that non cs people can use this too :)
+#-1 indicates no their are no possible sell/buy dates
+#This solution uses dynamic programming and has an worst case asymtotic time complexity of O(n) and a space complexity of O(1)
+
+
 def maxProfit(prices):
     cur_max_profit = 0
     if len(prices) == 0:
@@ -13,6 +20,7 @@ def maxProfit(prices):
             cur_max_profit = prices[day] - cur_min_price
             cur_max_idx = day + 1
     return (cur_max_profit,cur_min_idx,cur_max_idx)
+
 
 def main():
     prices = [1 , 1, 3, 5, 0, 7]
